25cc4c9e937fb2acf65582888c68f8289188d378
gaojieqing <1009192006@qq.com>
2017-10-29 22:33:17 +0800

    整合Mybatis

---
+++ src/main/java/com/gjq/repository/dao/IUserDao.java
@@ -0,0 +1,17 @@
+package com.gjq.repository.dao;
+
+import com.gjq.repository.pojo.User;
+
+public interface IUserDao {
+	int deleteByPrimaryKey(Integer id);
+
+	int insert(User record);
+
+	int insertSelective(User record);
+
+	User selectByPrimaryKey(Integer id);
+
+	int updateByPrimaryKeySelective(User record);
+
+	int updateByPrimaryKey(User record);
+}
---
+++ src/main/java/com/gjq/repository/service/IUserService.java
@@ -0,0 +1,7 @@
+package com.gjq.repository.service;
+
+import com.gjq.repository.pojo.User;
+
+public interface IUserService {
+	public User getUserById(int userId);
+}
